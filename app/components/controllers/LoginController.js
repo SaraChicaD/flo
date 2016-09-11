3b4cdd60b36d32844e2d517a184ba7b964cf234c
Holly Gibson <hollyglotapps@gmail.com>
2016-09-11 12:48:19 -0500

    Checks for flo and creates it

--- app/components/controllers/LoginController.js
+++ app/components/controllers/LoginController.js
@@ -42,9 +42,19 @@ angular.module('flo.login', ['ngRoute', 'datePicker'])
         gapi.client.load('calendar', 'v3', function() {
             var request = gapi.client.calendar.calendarList.list();
             request.execute(function(resp){
+								console.log('resp', resp);
                 $.each( resp.items, function( key, value ) {
                     console.log(resp.items[key].id);
                 });
+
+
+								request.execute(function(resp) {
+										var flo = resp.items.filter(function(item) {
+											return item.summary === 'Flo';
+										})
+
+										console.log('flo', flo);
+		            });
             });
             var request1 = gapi.client.calendar.events.list({
                 'calendarId': 'primary',
@@ -63,10 +73,10 @@ angular.module('flo.login', ['ngRoute', 'datePicker'])
 
     $scope.createEvent=function () {
 
-        var startDate = '2016-08-10T09:00:00-07:00';
-        var endDate = '2016-08-16T09:00:00-07:00';
+        var startDate = '2016-09-12T09:00:00-07:00';
+        var endDate = '2016-09-16T09:00:00-07:00';
 
-        //console.log('\n\ncreating event');
+        console.log('\n\ncreating event');
         var event = {
           'summary': 'Period #MyFlo',
           'location': 'Wherever I am',
@@ -93,20 +103,44 @@ angular.module('flo.login', ['ngRoute', 'datePicker'])
 
         gapi.client.load('calendar', 'v3', function() {
 
-            var request = gapi.client.calendar.events.insert({
-              'calendarId': 'primary',
-              'resource': event
-            });
+						var request = gapi.client.calendar.calendarList.list();
 
-            console.log('request', request);
+						request.execute(function(resp){
+							// Check if Flo is present, else create it
+							var flo = resp.items.filter(function(item) {
+								return item.summary === 'Flo';
+							})
+							if (flo.length === 0) {
+								gapi.client.calendar.calendars.insert({
+									summary: 'Flo'
+								}).execute();
+							}
+						});
 
-            request.execute(function(event) {
-              $('.event').append('Event created: ' + event.htmlLink);
+						var request = gapi.client.calendar.calendarList.list();
+						request.execute(function(resp){
+							// Check if Flo is present, else create it
+							var floCal = resp.items.filter(function(item) {
+								return item.summary === 'Flo';
+							});
 
-              console.log('event', event);
-            });
+							var floId = floCal[0].id;
 
-            console.log('created at the bottom ');
+							var request = gapi.client.calendar.events.insert({
+								'calendarId': floId,
+								'resource': event
+							});
+							console.log('request', request);
+
+							request.execute(function(event) {
+								$('.event').append('Event created: ' + event.htmlLink);
+
+								console.log('event', event);
+							});
+
+							console.log('created at the bottom ');
+
+						});
         });
 
     }
